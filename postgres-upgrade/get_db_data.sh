81a47e3b31d6bfb5216763e050856563613ddcb0
Praful Rakhade <prafulrakhade02@gmail.com>
2025-12-03 18:05:22 +0530

    Add branch handling for digital-card-service
    Signed-off-by: Praful Rakhade <prafulrakhade02@gmail.com>

--- postgres-upgrade/get_db_data.sh
+++ postgres-upgrade/get_db_data.sh
@@ -53,6 +53,10 @@ while IFS= read -r line; do
   then
   branch=release-1.3.x
   git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
+  elif [ $REPO_NAME == "digital-card-service" ]
+  then
+  branch=release-1.3.x
+  git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
   else
   git clone -b $1 https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
   fi
