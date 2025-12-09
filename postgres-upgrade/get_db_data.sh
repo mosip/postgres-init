b2c066f95def0a382343b97d184c55ca2a681ad9
Mohan E <mohanraj1715@gmail.com>
2025-12-09 17:02:48 +0530

    [MOSIP-43640] Updated get_db_data.sh
    Signed-off-by: Mohan E <mohanraj1715@gmail.com>

--- postgres-upgrade/get_db_data.sh
+++ postgres-upgrade/get_db_data.sh
@@ -39,7 +39,7 @@ while IFS= read -r line; do
   git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
   elif [ $REPO_NAME == "partner-management-services" ]
   then
-  branch=v1.2.2.2
+  branch=release-1.2.2.x
   git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
   elif [ $REPO_NAME == "pre-registration" ]
   then
